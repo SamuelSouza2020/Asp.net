5e27e4ef56d88ee1179af4518f1f2d5ab4cbdd35
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-09-04 18:12:25 -0300

    Razor5

---
+++ RazorStudy/RazorStudy/Pages/default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorStudy.Pages
+{
+    public class defaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
