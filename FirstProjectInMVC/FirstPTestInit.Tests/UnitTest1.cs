fda4d44031924987b2161a384ea29873381e73a6
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-09-07 15:57:19 -0300

    initial MVC

---
+++ FirstProjectInMVC/FirstPTestInit.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FirstPTestInit.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
