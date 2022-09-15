7d91b306f8b90cd0735b57f02879a5db805c0c69
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-09-15 12:08:40 -0300

    crud

---
+++ Crud22/Crud22/Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud22.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud22;Integrated Security=True");
+        }
+    }
+}
---
+++ Crud22/Crud22/Models/Clientes.cs
@@ -0,0 +1,13 @@
+﻿namespace Crud22.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
